ad938485d93bab229fcbe8e0cde52a137ef544d2
michischeid <michischeid@web.de>
2016-01-23 21:35:10 +0100

    participation view created

--- src/js/controllers/event-ctrl.js
+++ src/js/controllers/event-ctrl.js
@@ -3,15 +3,16 @@
  */
 
 angular.module('RDash')
-    .controller('EventCtrl', ['$scope', '$cookieStore', EventCtrl]);
+    .controller('EventCtrl', ['$scope', '$cookieStore', 'ngDialog', EventCtrl]);
 
-function EventCtrl($scope, $cookieStore) {
-    this.events = [{
-        name: "Motorrad Klassik",
-        location: "Walldürn",
-        from: new Date("2016-06-03"),
-        to: new Date("2016-06-06")
-    },
+function EventCtrl($scope, $cookieStore, ngDialog) {
+    this.events = [
+        {
+            name: "Motorrad Klassik",
+            location: "Walldürn",
+            from: new Date("2016-06-03"),
+            to: new Date("2016-06-06")
+        },
         {
             name: "Motorrad Klassik",
             location: "Walldürn",
@@ -24,5 +25,48 @@ function EventCtrl($scope, $cookieStore) {
             from: new Date("2016-06-03"),
             to: new Date("2016-06-06")
         }];
+
+    this.openParticipationPopup = function (event) {
+        this.activeEvent=event;
+        ngDialog.open({
+            template: 'templates/participationPopup.html',
+            scope: $scope
+        });
+    };
+    this.motorcycles = [{
+        brand: "Yamaha",
+        year: 1997,
+        model: "TRX 850",
+        cyclinderCnt: 2,
+        capacity: 850,
+        selected: false
+    },
+        {
+            brand: "Yamaha",
+            year: 1997,
+            model: "TRX 860",
+            cyclinderCnt: 2,
+            capacity: 850,
+            selected: false
+        },
+        {
+            brand: "Yamaha",
+            year: 1997,
+            model: "TRX 870",
+            cyclinderCnt: 2,
+            capacity: 850,
+            selected: false
+        }];
+
+    this.selectedMotorcycles =[];
+    this.germanControls = {
+        selectAll       : "Alle auswählen",
+        selectNone      : "Keines auswählen",
+        reset           : "Alle zurücksetzen",
+        search          : "Suchen...",
+        nothingSelected : "Keines selektiert"         //default-label is deprecated and replaced with this.
+    }
+
+
 
 }
