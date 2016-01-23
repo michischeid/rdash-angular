70050ad084695f38ee226870c0935c13a5f63f76
michischeid <michischeid@web.de>
2016-01-24 00:25:23 +0100

    changes

--- src/js/directives/widget-header.js
+++ src/js/directives/widget-header.js
@@ -13,10 +13,11 @@ function rdWidgetTitle() {
             title: '@',
             icon: '@',
             status:'=',
-            subtitle:'@'
+            subtitle:'@',
+            location:'@'
         },
         transclude: true,
-        template: '<div uib-accordion-heading><div class="row"><div class="pull-left"><i class="fa" ng-class="icon"></i> <span style="font-size: 20px">{{title}}</span> <span ng-hide="!subtitle" ><br/><h5>{{subtitle}}</h5></span></div><div class="pull-right col-xs-6 col-sm-4"><i class="pull-right glyphicon" ng-class="{\'glyphicon-chevron-down\': status, \'glyphicon-chevron-right\': !status}"></i></div></div></div>',
+        template: '<div uib-accordion-heading><div class="row"><div class="pull-left"><i class="fa" ng-class="icon"></i> <span style="font-size: 20px">{{title}} <a href="http://maps.google.com/maps?q={{location}}">{{location}}</a></span> <span ng-hide="!subtitle" ><br/><h5>{{subtitle}}</h5></span></div><div class="pull-right col-xs-6 col-sm-4"><i class="pull-right glyphicon" ng-class="{\'glyphicon-chevron-down\': status, \'glyphicon-chevron-right\': !status}"></i></div></div></div>',
         restrict: 'E'
     };
     return directive;
