0d1e16c6a387514db75f0767d2bcf193e509251f
michischeid <michischeid@web.de>
2016-02-21 21:34:00 +0100

    motorcycle changes

--- src/js/controllers/motorcycle-ctrl.js
+++ src/js/controllers/motorcycle-ctrl.js
@@ -6,28 +6,39 @@ angular.module('Portal')
     .controller('MotorcycleCtrl', ['$scope', '$cookieStore', 'Motorcycle', MotorcycleCtrl]);
 
 function MotorcycleCtrl($scope, $cookieStore, Motorcycle) {
-    this.newMotorcycle={};
+    this.newMotorcycle={
+        brand:"",
+        model:"",
+        year:null,
+        capacity:null,
+        cylinderCnt:null,
+        class:null
+    };
     this.motorcycles = Motorcycle.query();
 
 
     this.saveMotorcycle = function (motorcycle) {
         var obj = Motorcycle.save(motorcycle);
+        console.log(JSON.stringify(obj));
         motorcycle.id=obj.id;
         motorcycle.status=false;
-        $scope.$apply();
-
     };
     this.deleteMotorcycle = function (motorcycle,$index) {
         Motorcycle.remove(motorcycle);
         this.motorcycles.splice($index,1);
-        $scope.$apply();
     };
     this.addMotorcycle = function (motorcycle) {
         Motorcycle.add(motorcycle);
-        this.newMotorcycle={};
+        this.newMotorcycle={
+            brand:"",
+            model:"",
+            year:null,
+            capacity:null,
+            cylinderCnt:null,
+            class:null
+        };
         this.motorcycles = Motorcycle.query();
-        motorcycle.status=false;
-        $scope.$apply();
+        this.newMotorcycle.status=false;
     };
 
 
