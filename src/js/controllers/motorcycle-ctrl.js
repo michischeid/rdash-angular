5db46f456619746f665965b4a9e2bc227bfe06b6
michischeid <michischeid@web.de>
2016-03-06 13:54:45 +0100

    changed saving motorcycle

--- src/js/controllers/motorcycle-ctrl.js
+++ src/js/controllers/motorcycle-ctrl.js
@@ -6,53 +6,45 @@ angular.module('Portal')
     .controller('MotorcycleCtrl', ['$scope', '$cookieStore', 'Motorcycle', MotorcycleCtrl]);
 
 function MotorcycleCtrl($scope, $cookieStore, Motorcycle) {
-    this.newMotorcycle={
-        brand:"",
-        model:"",
-        year:"",
-        capacity:"",
-        cylinderCnt:"",
-        class:"",
-        serialNo:""
+    var ctrl = this;
+    this.newMotorcycle = {
+        brand: "",
+        model: "",
+        year: "",
+        capacity: "",
+        cylinderCnt: "",
+        class: "",
+        serialNo: ""
     };
     this.motorcycles = Motorcycle.query();
 
-    this.collapse = function (motorcycle){
-        motorcycle.status=false;
+    this.collapse = function (motorcycle) {
+        motorcycle.status = false;
     }
-    this.getFormId = function (motorcycle){
-        var oldId = motorcycle.oldId;
-        if(!oldId){
-            oldId= motorcycle.id;
-        }
-        return oldId;
-    }
+
     this.saveMotorcycle = function (motorcycle) {
-        var oldId = motorcycle.oldId;
-        if(!oldId){
-           oldId= motorcycle.id;
-        }
-        motorcycle.$save(function(response){
-            console.log(response);
+        motorcycle.$save(function (response) {
+            motorcycle.oldId = oldId;
+            ctrl.motorcycles = Motorcycle.query();
         });
-        motorcycle.status=false;
-        motorcycle.oldId=oldId;
+       this.collapse();
+
     };
-    this.deleteMotorcycle = function (motorcycle,$index) {
+    this.deleteMotorcycle = function (motorcycle, $index) {
         motorcycle.$remove();
-        this.motorcycles.splice($index,1);
+        this.motorcycles.splice($index, 1);
     };
     this.addMotorcycle = function (motorcycle) {
         Motorcycle.add(motorcycle);
-        this.newMotorcycle={
-            brand:"",
-            model:"",
-            year:"",
-            capacity:"",
-            cylinderCnt:"",
-            class:"",
-            serialNo:"",
-            status:false
+        this.newMotorcycle = {
+            brand: "",
+            model: "",
+            year: "",
+            capacity: "",
+            cylinderCnt: "",
+            class: "",
+            serialNo: "",
+            status: false
         };
         this.motorcycles = Motorcycle.query();
     };
